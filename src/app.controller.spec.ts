a1efd221525740390741c753da78663ac3a30f21
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-15 20:03:46 -0300

    config: module path @backend

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@backend/app.controller';
+import { AppService } from '@backend/app.service';
 
 describe('AppController', () => {
   let appController: AppController;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@backend/app.controller';
+import { AppService } from '@backend/app.service';
 
 @Module({
   imports: [],
