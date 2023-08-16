aff3e5355f3158e481257abb9fc2497b8982b488
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-15 21:50:22 -0300

    feat(bootstrap): setup hostname and port from host

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@backend/app.module';
+import { env } from './app.env';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('bootstrap');
+  await app.listen(env.PORT,env.HOST,()=>{
+    logger.log(`Server running on http://${env.HOST}:${env.PORT}`);
+  });
 }
 bootstrap();
