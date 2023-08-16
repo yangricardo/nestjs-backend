f27dfd62bf9a331950f149c689b6395d5f59a000
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-15 21:41:45 -0300

    feat: load validated app env

---
+++ src/app.env.ts
@@ -0,0 +1,26 @@
+import { createZodDto } from 'nestjs-zod';
+import { z } from 'zod';
+import { StringIntNumberSchema } from './zod.schemas';
+
+export const AppEnvSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']).default('production'),
+  HOST: z.string().default('localhost'),
+  PORT: StringIntNumberSchema.default(3000),
+  DATABASE_URL: z
+    .string()
+    .url()
+    .default('postgres://postgres:postgres@localhost:5432/postgres'),
+});
+
+export class AppEnv extends createZodDto(AppEnvSchema) {}
+
+export const loadAppEnv = () => {
+  const env = AppEnvSchema.safeParse(process.env);
+  if (!env.success) {
+    console.error(env.error);
+    process.exit(1);
+  }
+  return env.data;
+}
+
+export const env = loadAppEnv();
