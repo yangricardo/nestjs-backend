2789a40b75ef77a47492b3b2a6d2a4e36c95d86f
Yang Ricardo Barcellos Miranda <yangricardo17@gmail.com>
2023-08-15 22:25:36 -0300

    feat: AppService

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,27 +1,10 @@
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { PrismaService } from 'nestjs-prisma';
-import { ExtendedPrismaClient, extendPrismaService } from './prisma.extended';
 
 @Injectable()
-export class AppService implements OnModuleInit{
-
+export class AppService implements OnModuleInit {
   private static logger = new Logger(AppService.name);
-  private readonly prismaService: ExtendedPrismaClient;
 
-  constructor(
-    prismaService: PrismaService
-  ) {
-    this.prismaService = extendPrismaService(prismaService);
-  }
-  async onModuleInit ()
-  {
+  async onModuleInit() {
     AppService.logger.log('onModuleInit');
-    await this.prismaService.user.paginate({
-      limit:1
-    });
-  }
-
-  getHello(): string {
-    return 'Hello World!';
   }
 }
